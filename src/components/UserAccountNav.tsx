b7d3d6cf59ddffdffbf12eb5b9a06c941cef520a
samuel_ndjouli <samuelndj.997@gmail.com>
2023-10-28 16:40:54 +0200

    imported UserAvatar component, and Lucide's LogOut icon

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -5,6 +5,9 @@ import React from 'react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
 import Link from 'next/link';
 import { signOut } from 'next-auth/react';
+import { Button } from './ui/button';
+import { LogOut } from 'lucide-react';
+import UserAvatar from './UserAvatar';
 
 type Props = {
     user: Pick<User, "name" | "image" | "email">;
@@ -15,6 +18,7 @@ const UserAccountNav = ({user}: Props) => {
     <DropdownMenu>
         <DropdownMenuTrigger>
             {/* user avatar */}
+            <UserAvatar user={user} />
         </DropdownMenuTrigger>
         <DropdownMenuContent className="bg-white" align="end">
             <div className="flex items-center justify-start gap-2 p-2">
@@ -36,11 +40,15 @@ const UserAccountNav = ({user}: Props) => {
             </DropdownMenuItem>
 
             <DropdownMenuSeparator />
-            <DropdownMenuItem asChild onClick={(e) => {
+            <DropdownMenuItem 
+                onClick={(e) => {
                 e.preventDefault()
                 signOut().catch(console.error)
-            }}>
+            }}
+            className="text-red-600 cursor-pointer"
+            >
                 Sign out
+                <LogOut className="w-4 h-4 ml-2" />
             </DropdownMenuItem>
 
         </DropdownMenuContent>
