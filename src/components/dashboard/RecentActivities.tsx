2191a662419a00f05771264f95ef5e9816b3a700
samuel_ndjouli <samuelndj.997@gmail.com>
2023-10-29 16:37:42 +0100

    created a RecentActivities component

---
+++ src/components/dashboard/RecentActivities.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
+
+type Props = {}
+
+const RecentActivities = (props: Props) => {
+  return (
+    <Card className="col-span-4 lg:col-span-3">
+       <CardHeader>
+            <CardTitle className="text-2xl font-bold">
+                Recent Activities
+            </CardTitle>
+            <CardDescription>
+                You have played a total of 7 games.
+            </CardDescription>
+       </CardHeader>
+
+       <CardContent className="max-h-[500px] overflow-scroll">
+            Historiess
+       </CardContent>
+    </Card>
+  )
+}
+
+export default RecentActivities
