86c861aad39a5fda0a0c67cf25d2b8a00ce7cac6
samuel_ndjouli <samuelndj.997@gmail.com>
2023-10-30 15:12:20 +0100

    added some values to the CustomWordCloud component

--- src/components/CustomWordCloud.tsx
+++ src/components/CustomWordCloud.tsx
@@ -11,7 +11,27 @@ const data = [
         text: "Hey",
         value: 3,
     },
+    {
+        text: "Hi",
+        value: 5,
+    },
+    {
+        text: "computer",
+        value: 10,
+    },
+    {
+        text: "nextjs",
+        value: 8,
+    },
+    {
+        text: "live",
+        value: 7,
+    },
 ]
+
+const fontSizeWrapper = (word: {value: number}) => {
+    return Math.log2(word.value) * 5 + 16;
+}
 
 const CustomWordCloud = (props: Props) => {
     const theme = useTheme();
@@ -21,7 +41,7 @@ const CustomWordCloud = (props: Props) => {
             height={550}
             font="Times"
             data={data}
-            fontSize={9}
+            fontSize={fontSizeWrapper}
             rotate={0}
             padding={10}
             fill = {theme.theme == "dark" ? "white" : "black"}
